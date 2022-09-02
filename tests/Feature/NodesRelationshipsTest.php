cc6f7961284d922af518bf73b56e7f54211e6952
medilies <medilies@gmail.com>
2022-09-02 18:09:43 +0100

    Test nodes relationships

---
+++ tests/Feature/NodesRelationshipsTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Tests\Feature;
+
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use App\Models\Graph;
+use App\Models\Node;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class NodesRelationshipsTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+    /** @test */
+    public function nodes_test()
+    {
+        $graph = Graph::factory()->create();
+
+        $node = $graph->nodes()->create();
+
+        $child_node = $node->childNodes()->create(['graph_id' => $graph->id]);
+
+        $this->assertDatabaseCount('nodes', 2);
+
+        // dump(Node::with(['childNodes', 'parentNodes'])->get()->toArray());
+
+        $this->assertEquals($node->id, $child_node->id - 1);
+
+        $this->assertEquals($node->childNodes->first()->id, $child_node->id);
+
+        $this->assertEquals($child_node->ParentNodes->first()->id, $node->id);
+    }
+
+    /** @test */
+    public function child_nodes_factory_test()
+    {
+        $graph = Graph::factory()->create();
+
+        $node = Node::factory()
+            ->for($graph)
+            ->has(
+                Node::factory()->for($graph),
+                'childNodes'
+            )
+            ->create();
+
+        $this->assertDatabaseCount('nodes', 2);
+
+        $this->assertEquals($node->id, --$node->childNodes()->first()->id);
+    }
+
+    /** @test */
+    public function nodes2_factory_test()
+    {
+        $graph = Graph::factory()->create();
+
+        $node = Node::factory()
+            ->for($graph)
+            ->create();
+
+        // $child_node = Node::factory()
+        //     ->for($graph)
+        //     ->for($node, 'parentNodes')
+        //     ->create();
+
+        // $this->assertDatabaseCount('nodes', 2);
+
+        // $this->assertEquals($node->id, --$node->childNodes()->first()->id);
+    }
+}
